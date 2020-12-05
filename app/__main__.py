a0b6e973440633749982a0f815b7ae6c5ec44ca2
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2020-12-05 19:49:58 +0100

    Add Test command to test current message sending idea

--- app/__main__.py
+++ app/__main__.py
@@ -4,11 +4,33 @@ from app.messaging.commands import CommandMapper
 from app.messaging.command_handler import CommandHandler
 from app.messaging.broker import Broker
 
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from app.messaging.commands import Command
+from app.infrastructure.message import ContentType
+from typing import Any, List
+
+@dataclass_json
+@dataclass(frozen=True)
+class TestCommand(Command):
+    content: any
+    content_type: ContentType
+
+    @classmethod
+    def get_identifier(cls) -> str:
+        return 'TEST'
+
+    def invoke(self, receiver: Any) -> List[Command]:
+        print('Hello there')
+
 if __name__ == '__main__':
     server_settings = ConnectionSettings('127.0.0.1', 39123)
     command_mapper = CommandMapper()
-    command_handler = CommandHandler()
+    command_mapper.register(TestCommand)
+    command_handler = CommandHandler(command_mapper)
+    command_handler.register(TestCommand, None)
     broker = Broker(command_mapper, command_handler)
+    broker.start()
     tor_server = TorServer(server_settings, broker)
     tor_server.start()
     tor_service = TorService(server_settings)
