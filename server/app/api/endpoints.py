d86de852742f3008a02da9953e36c2f6b7d04ef8
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2020-12-07 19:06:40 +0100

    Add CORS

--- server/app/api/endpoints.py
+++ server/app/api/endpoints.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from app.api.contact_resource import ContactResource
 from app.api.message_resource import MessageResource
 
+
 app = Flask(__name__)
+CORS(app)
 api = Api()
 
 api.add_resource(ContactResource, '/api/contacts', '/api/contacts/', '/api/contacts/<id>')
