31285b532782f00c6e95538488646df3a5e579f8
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2020-12-13 15:36:52 +0100

    Remove flask_restful, add marshmallow

---
+++ server/app/api/custom_fields.py
@@ -0,0 +1,9 @@
+from marshmallow import fields, validate, ValidationError
+
+class BytesField(fields.Field):
+    def _validate(self, value):
+        if not isinstance(value, bytes):
+            raise ValidationError('Invalid input type.')
+
+        if value is None or value == b'':
+            raise ValidationError('Invalid value')
---
+++ server/app/api/schemas.py
@@ -0,0 +1,34 @@
+from marshmallow import Schema, fields, validate, pre_load, post_dump, post_load, ValidationError
+
+from app.api.custom_fields import BytesField
+from app.infrastructure.contact import Contact
+from app.infrastructure.message import Message
+
+
+class ContactSchema(Schema):
+    __model__ = Contact
+
+    contact_id = fields.Str()
+    name = fields.Str()
+    approved = fields.Bool()
+    awaiting_approval = fields.Bool()
+    address = fields.Str()
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        return self.__model__(**data)
+
+
+class MessageSchema(Schema):
+    __model__ = Message
+
+    contact = fields.Nested(ContactSchema())
+    content = BytesField()
+    content_type = fields.Int()
+    timestamp = fields.DateTime()
+    message_author = fields.Int()
+    message_state = fields.Int()
+
+    @post_load
+    def make_object(self, data, **kwargs):
+        return self.__model__(**data)
