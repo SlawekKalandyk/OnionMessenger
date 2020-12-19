a5b8f74788b2d5b26112394d3e05a3f5f7cb37a1
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2020-12-19 19:57:34 +0100

    Add socket-io for frontend/backend synchronisation

---
+++ angular-client/src/app/services/socket-service/socket.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as socketio from 'socket.io-client';
+import { ContactDto } from 'src/app/data-transfer/contact-dto';
+import { MessageDto } from 'src/app/data-transfer/message-dto';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket: any;
+
+  constructor() {
+    this.socket = socketio.io(environment.socket_url);
+  }
+
+  getMessages(): Observable<MessageDto> {
+    return new Observable(observer => {
+      this.socket.on('message', (message: MessageDto) => {
+        observer.next(message);
+      });
+    });
+  }
+
+  getContacts(): Observable<ContactDto> {
+    return new Observable(observer => {
+      this.socket.on('contact', (contact: ContactDto) => {
+        observer.next(contact);
+      });
+    });
+  }
+}
