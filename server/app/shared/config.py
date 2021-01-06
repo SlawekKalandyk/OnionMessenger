64f77abdd428012ae403246849c3dcf54a342e40
Slawomir Kalandyk <slawomir.kalandyk@gmail.com>
2021-01-06 15:04:09 +0100

    Add authentication/signature file saving

--- server/app/shared/config.py
+++ server/app/shared/config.py
@@ -72,3 +72,46 @@ class DatabaseConfiguration(BaseConfiguration):
             os.makedirs(DatabaseConfiguration._get_local_app_full_path())
 
         return os.path.join(DatabaseConfiguration._get_local_app_full_path(), DatabaseConfiguration.database_name)
+
+
+class AuthenticationConfiguration(BaseConfiguration):
+    signature_private_key_file_name = 'signature_private_key'
+    signature_public_key_file_name = 'signature_public_key'
+    signature_private_key = None
+    signature_public_key = None
+
+    @staticmethod
+    def save_signature_private_key(new_signature_private_key):
+        with open(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_private_key_file_name), 'w+') as signature_private_key_file:
+            signature_private_key_file.write(new_signature_private_key)
+            AuthenticationConfiguration.signature_private_key = new_signature_private_key
+
+    @staticmethod
+    def get_signature_private_key():
+        if AuthenticationConfiguration.signature_private_key:
+            return AuthenticationConfiguration.signature_private_key
+
+        if not os.path.exists(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_private_key_file_name)):
+            return None
+
+        with open(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_private_key_file_name), 'r') as signature_private_key_file:
+            AuthenticationConfiguration.signature_private_key = signature_private_key_file.readline()
+            return AuthenticationConfiguration.signature_private_key
+
+    @staticmethod
+    def save_signature_public_key(new_signature_public_key):
+        with open(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_public_key_file_name), 'w+') as signature_public_key_file:
+            signature_public_key_file.write(new_signature_public_key)
+            AuthenticationConfiguration.signature_public_key = new_signature_public_key
+
+    @staticmethod
+    def get_signature_public_key():
+        if AuthenticationConfiguration.signature_public_key:
+            return AuthenticationConfiguration.signature_public_key
+        
+        if not os.path.exists(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_public_key_file_name)):
+            return None
+
+        with open(os.path.join(AuthenticationConfiguration._get_local_app_full_path(), AuthenticationConfiguration.signature_public_key_file_name), 'r') as signature_public_key_file:
+            AuthenticationConfiguration.signature_public_key = signature_public_key_file.readline()
+            return AuthenticationConfiguration.signature_public_key
